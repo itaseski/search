e2a31b126152f867cde96c6c965439d9ad31da72
itaseski <itaseski@t.mk>
2023-02-22 18:18:57 +0100

    end

--- parts/admin.py
+++ parts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Part
+
+
+class PartAdmin(admin.ModelAdmin):
+    list_display = ("part_number", "designation",)
+
+admin.site.register(Part, PartAdmin)
